e489175146ca3f48f6bd36bb530f54cf68f5daf0
developmentCIFA <nicolappp71.np@gmail.com>
2026-03-04 11:48:44 +0100

    descrizione della modifica

--- main/credential.h
+++ main/credential.h
@@ -2,8 +2,8 @@
 #include "mode.h"
 
 #ifdef CASA
-#define CONFIG_WIFI_SS "DOMO_2.4G"
-#define CONFIG_WIFI_PASSWORD "DOMO2020tica"
+#define CONFIG_WIFI_SS "########"
+#define CONFIG_WIFI_PASSWORD "########"
 #else
 #define CONFIG_WIFI_SS "CIFAdomo"
 #define CONFIG_WIFI_PASSWORD "domo18tica"
