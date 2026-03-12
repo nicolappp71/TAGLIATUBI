c73f7c133c8ae7ee75d4f0af7b1937e3872e9659
developmentCIFA <nicolappp71.np@gmail.com>
2026-03-12 19:41:36 +0100

    demo

--- main/credential.h
+++ main/credential.h
@@ -3,7 +3,7 @@
 
 #ifdef CASA
 #define CONFIG_WIFI_SS "####"
-#define CONFIG_WIFI_PASSWORD "####"
+#define CONFIG_WIFI_PASSWORD "1111"
 #else
 #define CONFIG_WIFI_SS "CIFAdomo"
 #define CONFIG_WIFI_PASSWORD "domo18tica"
